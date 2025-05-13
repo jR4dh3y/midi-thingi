27e0dfab42e87637cbbe64380cb98ccc14d59e3b
Radhey Kalra <radheykalra901@gmail.com>
2025-05-14 00:43:40 +0530

    implemented a preload script that gives communicatin btw main proceses and electron window

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,15 +1,25 @@
 import {app, BrowserWindow} from 'electron';
 import path from 'path';
 import { isDev } from './util.js';
+import { pollResources } from './resourceManager.js';
+import { getPreloadPath } from './pathResolver.js';
 
 
 // type test = string; 
 
 app.on('ready', () => {
-  const mainWindow = new BrowserWindow({});
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      preload: getPreloadPath(),
+    },
+  });
   if (isDev()){
     mainWindow.loadURL('http://localhost:3000');
   }else{
     mainWindow.loadFile(path.join(app.getAppPath() + '/dist-react/index.html' ));
   }
+
+  pollResources();
+
+
 });
